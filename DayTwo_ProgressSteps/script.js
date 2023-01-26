1ca705f76073c1bd265736e7a694d73d3117bb84
jhaganb <jhaganb@gmail.com>
2023-01-27 01:36:46 +0530

    Styles.css is made

--- DayTwo_ProgressSteps/script.js
+++ DayTwo_ProgressSteps/script.js
@@ -1,1 +1,35 @@
-console.log("@");
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+}
