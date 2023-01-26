5572e3cc3c5a1e65aeac553472e8ca6d7133af1f
jhaganb <jhaganb@gmail.com>
2023-01-27 01:54:27 +0530

    script.js is updated

--- DayTwo_ProgressSteps/script.js
+++ DayTwo_ProgressSteps/script.js
@@ -32,4 +32,18 @@ function update() {
       circle.classList.remove("active");
     }
   });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
