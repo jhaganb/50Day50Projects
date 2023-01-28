b6ba20cbeae94bd8c34899958d095048b13c95ae
jhaganb <jhaganb@gmail.com>
2023-01-29 00:21:38 +0530

    Script.js is added

--- DayFour_HiddenSearchWidget/script.js
+++ DayFour_HiddenSearchWidget/script.js
@@ -1,1 +1,8 @@
-console.log("@");
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
